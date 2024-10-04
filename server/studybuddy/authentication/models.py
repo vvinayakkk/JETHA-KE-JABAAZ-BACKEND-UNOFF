9a0de6f92c3c4db9cee547e63a62e197ce0efce5
javascript <ntpjc2vinayak@gmail.com>
2024-10-05 00:05:49 +0530

    entire backend working

---
+++ server/studybuddy/authentication/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
+from .managers import CustomUserManager
+
+# Define User Model to Reflect Updated Requirements
+class User(AbstractUser):
+    # Use CustomUserManager for managing User creation
+    objects = CustomUserManager()
+    
+    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=150, unique=True, null=True, blank=True)
+    # Identify if the user is a student or senior (mentor)
+    is_student = models.BooleanField(default=False)
+    is_senior = models.BooleanField(default=False)
+    
+    # Many-to-Many Relationship for Connections
+    
+    profile_image = models.ImageField(upload_to='profile_images/', null=True, blank=True)
+    friends = models.ManyToManyField('self', blank=True, symmetrical=False)
+
+    # Retain AbstractUser fields with custom `groups` and `user_permissions` fields
+    groups = models.ManyToManyField(
+        'auth.Group',
+        related_name='custom_user_set',
+        blank=True,
+        help_text=_('The groups this user belongs to. A user will get all permissions granted to each of their groups.'),
+        verbose_name=_('groups')
+    )
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        related_name='custom_user_set',
+        blank=True,
+        help_text=_('Specific permissions for this user.'),
+        verbose_name=_('user permissions')
+    )
+    
+    # Specify custom USERNAME_FIELD and REQUIRED_FIELDS
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    def __str__(self):
+        return self.email
